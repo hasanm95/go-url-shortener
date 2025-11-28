6c8dfb693136e1727fad755881b7ba0f61301dce
hasanm95 <hsncool10@gmail.com>
2025-11-28 17:56:14 +0800

    added basic router

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hasanm95/go-url-shortener/internal/config"
+)
 
 func main(){
-	fmt.Println("Logging main")
+	cfg := config.Load()
+
+	r := gin.Default()
+
+  // Define a simple GET endpoint
+  r.GET("/ping", func(c *gin.Context) {
+    // Return JSON response
+    c.JSON(http.StatusOK, gin.H{
+      "message": "pong",
+    })
+  })
+
+  port := fmt.Sprintf(":%s", cfg.Port)
+
+  log.Println("Server will run on port %v", port)
+
+  if err := r.Run(port); err != nil {
+    log.Fatalf("failed to run server: %v", err)
+  }
 }
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,9 @@
 package repository
+
+import "github.com/hasanm95/go-url-shortener/internal/models"
+
+type URLRespository interface {
+	Create(url *models.URL) error
+	GetByShortCode(shortCode string) (*models.URL, error)
+	IncrementClicks(shortCode string) error
+}
