5e10467289e066332e9cae888a7ccdfeb3ece89b
hasanm95 <hsncool10@gmail.com>
2025-11-28 17:01:07 +0800

    created db connection function

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -1,1 +1,23 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func NewPostgresDB(databseURL string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", databseURL)
+
+	if err != nil {
+		return nil, fmt.Errorf("error opening db: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("error connecting to db: %w", err)
+	}
+
+	log.Println("DB connected successfully")
+
+	return  db, nil;
+}
