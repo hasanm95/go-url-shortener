48b9ccdc27ab91f168f1f8262888e5e1d7351c52
hasanm95 <hsncool10@gmail.com>
2025-11-28 16:57:19 +0800

    created config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,32 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port string
+	DatabaseURL string
+}
+
+func Load() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	return &Config{
+		Port: getEnv("PORT", "8080"),
+		DatabaseURL: getEnv("DATABASE_URL", ""),
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+
+	return  defaultValue
+}
